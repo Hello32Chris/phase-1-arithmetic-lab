1003a75f95636ebb417dc0df05067e0c7dbcd552
Christopher Orth <christopherorth430@yahoo.com>
2023-08-21 19:56:14 -0400

    methritic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*1)+ 1;
+
+let num3 =16;
+let num4 =4;
+let mod = num3/num4;
+
+const set = (3, 8, 2, 3, 20);
+let max = Math.max(set);
